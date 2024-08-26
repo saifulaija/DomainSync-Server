4aa2b108841969e2b246682a70e1ec99b9622f1e
saifulaija <saifulaija2023@gmail.com>
2024-08-26 14:14:44 +0600

    initial setup done

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import app from './app';
+// import seedSuperAdmin from './app/DB';
+import config from './app/config';
+
+import mongoose from 'mongoose';
+
+async function server() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    // seedSuperAdmin();
+    app.listen(config.port, () => {
+      console.log(`DomainSync app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+server();
